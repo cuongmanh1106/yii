aa942163124b38329ffb5c9ef318f5acf01511de
Cuong Nguyen <cuong.nguyen@s3corp.com.vn>
2020-03-31 16:18:38 +0700

    first commit

---
+++ console/migrations/m200323_081101_tbl_province.php
@@ -0,0 +1,53 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m200323_081101_tbl_province
+ */
+class m200323_081101_tbl_province extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m200323_081101_tbl_province cannot be reverted.\n";
+
+        return false;
+    }
+
+    
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('{{%tbl_province}}', [
+            'pro_id' => $this->primaryKey(),
+            'province_name' => $this->string()->notNull()->unique(),
+            'status' => $this->smallInteger()->notNull()->defaultValue(10),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+        ], $tableOptions);
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%tbl_province}}');
+
+    }
+    
+}
